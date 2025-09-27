a89e6fe2c321ef26af426d38aeed32af3d0abddc
Borja Oviedo <borjaoviedo.dev@gmail.com>
2025-09-27 14:56:43 +0200

    feat(infra): added Project and ProjectMember entities to AppDbContext

--- api/src/Infrastructure/Data/AppDbContext.cs
+++ api/src/Infrastructure/Data/AppDbContext.cs
@@ -8,6 +8,8 @@ namespace Infrastructure.Data
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 
         public DbSet<User> Users { get; set; }
+        public DbSet<Project> Projects { get; set; }
+        public DbSet<ProjectMember> ProjectMembers { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
             => modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
