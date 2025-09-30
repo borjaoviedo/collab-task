245a969f0077c27d084ed19360d333c38df58949
Borja Oviedo <borjaoviedo.dev@gmail.com>
2025-09-30 14:47:40 +0200

    refactor(frontend): move main.tsx into app folder

---
+++ web/src/app/main.tsx
@@ -0,0 +1,14 @@
+// src/app/main.tsx
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { AppProviders } from './providers'
+import { AppRouter } from './router'
+import '@/styles.css'
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <AppProviders>
+      <AppRouter />
+    </AppProviders>
+  </StrictMode>
+)
