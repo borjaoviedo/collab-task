92b01401387ed5e10e206e35411a1a5588f21494
Borja Oviedo <borjaoviedo.dev@gmail.com>
2025-10-01 08:05:28 +0200

    chore(auth): hydrate auth store on app bootstrap

--- web/src/app/main.tsx
+++ web/src/app/main.tsx
@@ -3,7 +3,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { AppProviders } from './providers'
 import { AppRouter } from './router'
+import { hydrateAuthStoreOnBoot } from "@shared/store/auth.store"
 import '@/styles.css'
+
+hydrateAuthStoreOnBoot();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
