8bae38010264d3a11bd6b6da699dafc540a7bf19
Borja Oviedo <borjaoviedo.dev@gmail.com>
2025-10-01 08:47:51 +0200

    chore(web): add "@" path alias to vite.config.ts

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -6,6 +6,7 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
+      "@": path.resolve(__dirname, "src"),
       '@app': path.resolve(__dirname, 'src/app'),
       '@shared': path.resolve(__dirname, 'src/shared'),
       '@features': path.resolve(__dirname, 'src/features'),
