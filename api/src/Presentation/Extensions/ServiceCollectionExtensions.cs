e0f3ff2d97070c82a3c88ebed5425be4c865a085
Borja Oviedo <borjaoviedo.dev@gmail.com>
2025-10-02 20:02:07 +0200

    feat(api): add ServiceCollectionExtensions to register API-level dependencies

---
+++ api/src/Presentation/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,28 @@
+using Api.Auth;
+using Api.Errors;
+using Application.Common.Abstractions.Auth;
+using Infrastructure;
+
+namespace Api.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddApiLayer(
+            this IServiceCollection services,
+            IConfiguration config,
+            string connectionString)
+        {
+            services
+                .AddCorsPolicies(config)
+                .AddInfrastructure(connectionString)
+                .AddSwaggerWithJwt()
+                .AddJwtAuth(config)
+                .AddHttpContextAccessor()
+                .AddScoped<ICurrentUserService, CurrentUserService>()
+                .AddAppValidation()
+                .AddProblemDetailsAndExceptionMapping();
+
+            return services;
+        }
+    }
+}
