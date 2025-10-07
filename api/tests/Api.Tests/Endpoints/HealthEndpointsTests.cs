0ca5b63e0c3af4ff5bed7668778c3deb787991a7
borjaoviedo <borjaoviedo.dev@gmail.com>
2025-10-07 08:04:06 +0200

    refactor(test): remove Endpoints/Health and relocate HealthEndpointsTests in Endpoints/

--- api/tests/Api.Tests/Endpoints/HealthEndpointsTests.cs
+++ api/tests/Api.Tests/Endpoints/HealthEndpointsTests.cs
@@ -1,7 +1,7 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
 
-namespace Api.Tests.Endpoints.Health
+namespace Api.Tests.Endpoints
 {
     public class HealthEndpointsTests
     {
