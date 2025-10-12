b582f40f199940e31c847d75846c885e09da4170
borjaoviedo <borjaoviedo.dev@gmail.com>
2025-10-12 15:46:21 +0200

    refactor(application): remove unnecessary TaskItemDeleteDto

--- api/src/Application/TaskItems/DTOs/TaskItemDeleteDto.cs
+++
@@ -1,9 +0,0 @@
-
-namespace Application.TaskItems.DTOs
-{
-    public sealed class TaskItemDeleteDto
-    {
-        public Guid Id { get; set; }
-        public byte[] RowVersion { get; set; } = default!;
-    }
-}
