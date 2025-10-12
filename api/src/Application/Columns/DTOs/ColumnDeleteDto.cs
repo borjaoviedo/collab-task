52ed6a10200794ddd19a15ef859784263b5efcf3
borjaoviedo <borjaoviedo.dev@gmail.com>
2025-10-12 15:11:19 +0200

    refactor(application): remove unnecessary ColumnDeleteDto

--- api/src/Application/Columns/DTOs/ColumnDeleteDto.cs
+++
@@ -1,9 +0,0 @@
-
-namespace Application.Columns.DTOs
-{
-    public sealed class ColumnDeleteDto
-    {
-        public Guid Id { get; set; }
-        public byte[] RowVersion { get; set; } = default!;
-    }
-}
