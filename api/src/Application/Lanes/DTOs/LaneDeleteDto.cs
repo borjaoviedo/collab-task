8f5453fde4bba3f76d8c30c7856d24d85e40c614
borjaoviedo <borjaoviedo.dev@gmail.com>
2025-10-12 14:16:40 +0200

    refactor(application): remove unnecessary LaneDeleteDto

--- api/src/Application/Lanes/DTOs/LaneDeleteDto.cs
+++
@@ -1,9 +0,0 @@
-
-namespace Application.Lanes.DTOs
-{
-    public sealed class LaneDeleteDto
-    {
-        public Guid Id { get; set; }
-        public byte[] RowVersion { get; set; } = default!;
-    }
-}
