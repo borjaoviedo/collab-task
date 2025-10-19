dd17b9a7cba356754781f958b1c37ba6bd6c6357
borjaoviedo <borjaoviedo.dev@gmail.com>
2025-10-19 10:44:06 +0200

    refactor(api): rename HttpContext parameter from http to context in ResolveRowVersionAsync for naming consistency

--- api/src/Presentation/Helpers/ConcurrencyHelpers.cs
+++ api/src/Presentation/Helpers/ConcurrencyHelpers.cs
@@ -2,9 +2,9 @@ namespace Api.Helpers
 {
     public static class ConcurrencyHelpers
     {
-        public static async Task<byte[]> ResolveRowVersionAsync<T>( HttpContext http, Func<Task<T?>> loader, Func<T, byte[]> selector) where T : class
+        public static async Task<byte[]> ResolveRowVersionAsync<T>(HttpContext context, Func<Task<T?>> loader, Func<T, byte[]> selector) where T : class
         {
-            if (http.Items.TryGetValue("rowVersion", out var o) && o is byte[] rv && rv.Length > 0)
+            if (context.Items.TryGetValue("rowVersion", out var o) && o is byte[] rv && rv.Length > 0)
                 return rv;
 
             var entity = await loader();
