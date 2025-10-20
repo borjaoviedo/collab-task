fd68fbb1f0c5d42baf604338726c627b0c5af4c0
borjaoviedo <borjaoviedo.dev@gmail.com>
2025-10-20 10:39:47 +0200

    refactor(api): remove unused properties Email, Name, Role, and IsAuthenticated from ICurrentUserService

--- api/src/Application/Common/Abstractions/Auth/ICurrentUserService.cs
+++ api/src/Application/Common/Abstractions/Auth/ICurrentUserService.cs
@@ -1,11 +1,8 @@
+
 namespace Application.Common.Abstractions.Auth
 {
     public interface ICurrentUserService
     {
-        bool IsAuthenticated { get; }
         Guid? UserId { get; }
-        string? Email { get; }
-        string? Name { get; }
-        string? Role { get; }
     }
 }
