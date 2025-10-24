5db798cbd05dc9cfce8ee4165757401659810681
borjaoviedo <borjaoviedo.dev@gmail.com>
2025-10-24 13:36:13 +0200

    chore(tests): remove obsolete BaseTest.cs and FakeClock.cs from Application.Tests/Common

--- api/tests/Application.Tests/Common/Helpers/FakeClock.cs
+++
@@ -1,9 +0,0 @@
-using Application.Common.Abstractions.Time;
-
-namespace Application.Tests.Common.Helpers
-{
-    public sealed class FakeClock(DateTimeOffset now) : IDateTimeProvider
-    {
-        public DateTimeOffset UtcNow { get; } = now;
-    }
-}
