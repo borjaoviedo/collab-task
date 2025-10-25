b9b1f4ab05c97aedbe436573f4f770851dcd50ba
borjaoviedo <borjaoviedo.dev@gmail.com>
2025-10-25 10:51:04 +0200

    test(domain): refactor ProjectMemberTests to use shared defaults and TestTime; reduced duplication, improved readability, added Restore_Sets_RemovedAt_Null test

--- api/tests/Domain.Tests/Entities/ProjectMemberTests.cs
+++ api/tests/Domain.Tests/Entities/ProjectMemberTests.cs
@@ -2,62 +2,90 @@ using Domain.Entities;
 using Domain.Enums;
 using Domain.ValueObjects;
 using FluentAssertions;
+using TestHelpers;
+using TestHelpers.Time;
 
 namespace Domain.Tests.Entities
 {
     public class ProjectMemberTests
     {
-        private static byte[] Bytes(int n) => Enumerable.Repeat((byte)0xCD, n).ToArray();
+        private static readonly Guid _defaultProjectId = Guid.NewGuid();
+        private static readonly Guid _defaultUserId = Guid.NewGuid();
+        private static readonly ProjectRole _defaultRole = ProjectRole.Member;
+        private readonly ProjectMember _defaultProjectMember = ProjectMember.Create(
+            _defaultProjectId,
+            _defaultUserId,
+            _defaultRole);
 
         [Fact]
         public void Set_All_Core_Properties_Assigns_Correctly()
         {
-            var projectId = Guid.NewGuid();
-            var userId = Guid.NewGuid();
-            var role = ProjectRole.Member;
-            var removedAt = DateTimeOffset.UtcNow.AddDays(-1);
-            var rv = Bytes(8);
+            var projectMember = _defaultProjectMember;
+            var removedAt = TestTime.FromFixedMinutes(-10);
+            var rowVersion = TestDataFactory.Bytes(8);
 
-            var pm = ProjectMember.Create(projectId, userId, role);
-            pm.Remove(removedAt);
-            pm.SetRowVersion(rv);
+            projectMember.Remove(removedAt);
+            projectMember.SetRowVersion(rowVersion);
 
-            pm.ProjectId.Should().Be(projectId);
-            pm.UserId.Should().Be(userId);
-            pm.Role.Should().Be(role);
-            pm.JoinedAt.Should().NotBe(null);
-            pm.RemovedAt.Should().Be(removedAt);
-            pm.RowVersion.Should().BeSameAs(rv);
+            projectMember.ProjectId.Should().Be(_defaultProjectId);
+            projectMember.UserId.Should().Be(_defaultUserId);
+            projectMember.Role.Should().Be(_defaultRole);
+            projectMember.JoinedAt.Should().NotBe(null);
+            projectMember.RemovedAt.Should().Be(removedAt);
+            projectMember.RowVersion.Should().BeSameAs(rowVersion);
         }
 
         [Fact]
         public void Navigation_Properties_Assignable()
         {
-            var user = User.Create(Email.Create("member@demo.com"), UserName.Create("Project Member"), Bytes(32), Bytes(16));
-            var p = Project.Create(user.Id, ProjectName.Create("A Project Name"));
+            var user = User.Create(
+                email: Email.Create("member@demo.com"),
+                name: UserName.Create("Project Member"),
+                hash: TestDataFactory.Bytes(32),
+                salt: TestDataFactory.Bytes(16));
+            var project = Project.Create(user.Id, name: ProjectName.Create("A Project Name"));
+            var projectMember = ProjectMember.Create(project.Id, user.Id, _defaultRole);
 
-            var pm = ProjectMember.Create(p.Id, user.Id, ProjectRole.Reader);
-            pm.SetUser(user);
-            pm.SetProject(p);
+            projectMember.SetUser(user);
+            projectMember.SetProject(project);
 
-            pm.Project.Should().BeSameAs(p);
-            pm.User.Should().BeSameAs(user);
-            pm.ProjectId.Should().Be(p.Id);
-            pm.UserId.Should().Be(user.Id);
+            projectMember.Project.Should().BeSameAs(project);
+            projectMember.User.Should().BeSameAs(user);
+            projectMember.ProjectId.Should().Be(project.Id);
+            projectMember.UserId.Should().Be(user.Id);
         }
 
         [Fact]
-        public void Role_Can_Change()
+        public void ChangeRole_Changes_Role()
         {
-            var pm = ProjectMember.Create(Guid.NewGuid(), Guid.NewGuid(), ProjectRole.Reader);
+            var projectMember = _defaultProjectMember;
 
-            pm.Role.Should().Be(ProjectRole.Reader);
-            pm.ChangeRole(ProjectRole.Member);
-            pm.Role.Should().Be(ProjectRole.Member);
-            pm.ChangeRole(ProjectRole.Admin);
-            pm.Role.Should().Be(ProjectRole.Admin);
-            pm.ChangeRole(ProjectRole.Owner);
-            pm.Role.Should().Be(ProjectRole.Owner);
+            projectMember.Role.Should().Be(_defaultRole);
+
+            projectMember.ChangeRole(ProjectRole.Reader);
+            projectMember.Role.Should().Be(ProjectRole.Reader);
+
+            projectMember.ChangeRole(ProjectRole.Admin);
+            projectMember.Role.Should().Be(ProjectRole.Admin);
+
+            projectMember.ChangeRole(ProjectRole.Owner);
+            projectMember.Role.Should().Be(ProjectRole.Owner);
+        }
+
+        [Fact]
+        public void Restore_Sets_RemovedAt_Null()
+        {
+            var projectMember = _defaultProjectMember;
+
+            projectMember.RemovedAt.Should().BeNull();
+
+            var removedAt = TestTime.FromFixedMinutes(-5);
+            projectMember.Remove(removedAt);
+
+            projectMember.RemovedAt.Should().Be(removedAt);
+
+            projectMember.Restore();
+            projectMember.RemovedAt.Should().BeNull();
         }
     }
 }
