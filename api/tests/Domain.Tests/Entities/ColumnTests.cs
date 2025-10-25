9890db8d2a8bc9c498deed4802f60ef5dd500c95
borjaoviedo <borjaoviedo.dev@gmail.com>
2025-10-25 10:49:42 +0200

    test(domain): refactor ColumnTests to use shared default values and _defaultColumn; reduced duplication, clearer naming

--- api/tests/Domain.Tests/Entities/ColumnTests.cs
+++ api/tests/Domain.Tests/Entities/ColumnTests.cs
@@ -6,46 +6,64 @@ namespace Domain.Tests.Entities
 {
     public sealed class ColumnTests
     {
+        private static readonly Guid _defaultProjectId = Guid.NewGuid();
+        private static readonly Guid _defaultLaneId = Guid.NewGuid();
+        private static readonly ColumnName _defaultColumnName = ColumnName.Create("column");
+        private static readonly int _defaultColumnOrder = 0;
+        private readonly Column _defaultColumn = Column.Create(
+            _defaultProjectId,
+            _defaultLaneId,
+            _defaultColumnName,
+            _defaultColumnOrder);
+
         [Fact]
         public void Set_All_Core_Properties_Assigns_Correctly()
         {
-            var projectId = Guid.NewGuid();
-            var laneId = Guid.NewGuid();
-            var name = ColumnName.Create("Demo column");
             var order = 3;
+            var column = Column.Create(
+            _defaultProjectId,
+            _defaultLaneId,
+            _defaultColumnName,
+            order);
 
-            var c = Column.Create(projectId, laneId, name, order);
-
-            c.ProjectId.Should().Be(projectId);
-            c.LaneId.Should().Be(laneId);
-            c.Name.Should().Be(name);
-            c.Order.Should().Be(order);
+            column.ProjectId.Should().Be(_defaultProjectId);
+            column.LaneId.Should().Be(_defaultLaneId);
+            column.Name.Should().Be(_defaultColumnName);
+            column.Order.Should().Be(order);
         }
 
         [Fact]
         public void Column_Id_Is_Initialized()
         {
-            var c = Column.Create(Guid.NewGuid(), Guid.NewGuid(), ColumnName.Create("column"), 1);
+            var column = _defaultColumn;
 
-            c.Id.Should().NotBeEmpty();
-            c.Id.Should().NotBe(Guid.Empty);
+            column.Id.Should().NotBeEmpty();
+            column.Id.Should().NotBe(Guid.Empty);
         }
 
         [Fact]
         public void Column_Is_Initialized_When_Null_Order()
         {
-            var c = Column.Create(Guid.NewGuid(), Guid.NewGuid(), ColumnName.Create("column"), null);
+            var column = Column.Create(
+                _defaultProjectId,
+                _defaultLaneId,
+                _defaultColumnName,
+                order: null);
 
-            c.Name.Value.Should().Be("column");
+            column.Name.Value.Should().Be(_defaultColumnName.Value);
+            column.Order.Should().Be(_defaultColumnOrder);
         }
 
         [Fact]
         public void Column_Is_Initialized_With_Order_0_When_Negative_Order()
         {
-            var c = Column.Create(Guid.NewGuid(), Guid.NewGuid(), ColumnName.Create("column"), -1);
+            var column = Column.Create(
+                _defaultProjectId,
+                _defaultLaneId,
+                _defaultColumnName,
+                order: -1);
 
-            c.Name.Value.Should().Be("column");
-            c.Order.Should().Be(0);
+            column.Order.Should().Be(_defaultColumnOrder);
         }
 
         [Theory]
@@ -54,63 +72,87 @@ namespace Domain.Tests.Entities
         [InlineData("a")]
         public void Invalid_ColumnName_Throws(string input)
         {
-            Action act = () => Column.Create(Guid.NewGuid(), Guid.NewGuid(), ColumnName.Create(input), 1);
+            var act = () => Column.Create(
+                _defaultProjectId,
+                _defaultLaneId,
+                name: ColumnName.Create(input),
+                _defaultColumnOrder);
+
             act.Should().Throw<ArgumentException>();
         }
 
         [Fact]
         public void ProjectId_With_Guid_Empty_Throws()
         {
-            var projectId = Guid.Empty;
-            Action act = () => Column.Create(projectId, Guid.NewGuid(), ColumnName.Create("column"), 1);
+            var act = () => Column.Create(
+                projectId: Guid.Empty,
+                _defaultLaneId,
+                _defaultColumnName,
+                _defaultColumnOrder);
+
             act.Should().Throw<ArgumentException>();
         }
 
         [Fact]
         public void LaneId_With_Guid_Empty_Throws()
         {
-            var laneId = Guid.Empty;
-            Action act = () => Column.Create(Guid.NewGuid(), laneId, ColumnName.Create("column"), 1);
+            var act = () => Column.Create(
+                _defaultProjectId,
+                laneId: Guid.Empty,
+                _defaultColumnName,
+                _defaultColumnOrder);
+
             act.Should().Throw<ArgumentException>();
         }
 
         [Fact]
         public void Rename_Changes_Name()
         {
-            var c = Column.Create(Guid.NewGuid(), Guid.NewGuid(), ColumnName.Create("column"), 1);
-            c.Rename(ColumnName.Create("New column"));
-            c.Name.Value.Should().Be("New column");
+            var column = _defaultColumn;
+            var newColumnName = "new";
+
+            column.Rename(ColumnName.Create(newColumnName));
+
+            column.Name.Value.Should().Be(newColumnName);
         }
 
         [Fact]
         public void Rename_Same_Name_Does_Not_Change()
         {
-            var c = Column.Create(Guid.NewGuid(), Guid.NewGuid(), ColumnName.Create("column"), 1);
-            c.Rename(ColumnName.Create("column"));
-            c.Name.Value.Should().Be("column");
+            var column = _defaultColumn;
+
+            column.Rename(_defaultColumnName);
+
+            column.Name.Value.Should().Be(_defaultColumnName.Value);
         }
 
         [Fact]
         public void Reorder_Changes_Order()
         {
-            var c = Column.Create(Guid.NewGuid(), Guid.NewGuid(), ColumnName.Create("column"), 1);
-            c.Reorder(2);
-            c.Order.Should().Be(2);
+            var column = _defaultColumn;
+
+            column.Reorder(2);
+
+            column.Order.Should().Be(2);
         }
 
         [Fact]
         public void Reorder_Same_Order_Does_Not_Change()
         {
-            var c = Column.Create(Guid.NewGuid(), Guid.NewGuid(), ColumnName.Create("column"), 1);
-            c.Reorder(1);
-            c.Order.Should().Be(1);
+            var column = _defaultColumn;
+
+            column.Reorder(_defaultColumnOrder);
+
+            column.Order.Should().Be(_defaultColumnOrder);
         }
 
         [Fact]
         public void Reorder_Negative_Order_Throws()
         {
-            var c = Column.Create(Guid.NewGuid(), Guid.NewGuid(), ColumnName.Create("column"), 1);
-            Action act = () => c.Reorder(-1);
+            var column = _defaultColumn;
+
+            var act = () => column.Reorder(-1);
+
             act.Should().Throw<ArgumentOutOfRangeException>();
         }
     }
