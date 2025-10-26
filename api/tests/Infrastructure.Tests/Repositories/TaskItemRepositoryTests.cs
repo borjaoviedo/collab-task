d7036e78af860de3ce445c519e86a6fc83522780
borjaoviedo <borjaoviedo.dev@gmail.com>
2025-10-26 09:05:58 +0100

    test(infra): migrate TaskItemRepositoryTests to UnitOfWork and PrecheckStatus

--- api/tests/Infrastructure.Tests/Repositories/TaskItemRepositoryTests.cs
+++ api/tests/Infrastructure.Tests/Repositories/TaskItemRepositoryTests.cs
@@ -2,6 +2,7 @@ using Domain.Entities;
 using Domain.Enums;
 using Domain.ValueObjects;
 using FluentAssertions;
+using Infrastructure.Data;
 using Infrastructure.Data.Repositories;
 using Microsoft.EntityFrameworkCore;
 using TestHelpers;
@@ -16,14 +17,20 @@ namespace Infrastructure.Tests.Repositories
             using var dbh = new SqliteTestDb();
             await using var db = dbh.CreateContext();
             var repo = new TaskItemRepository(db);
+            var uow = new UnitOfWork(db);
 
             var (pId, lId, cId) = TestDataFactory.SeedLaneWithColumn(db);
             var taskTitle = "Task title";
             var taskDescription = "Description";
 
-            var task = TaskItem.Create(cId, lId, pId, TaskTitle.Create(taskTitle), TaskDescription.Create(taskDescription));
+            var task = TaskItem.Create(
+                cId,
+                lId,
+                pId,
+                TaskTitle.Create(taskTitle),
+                TaskDescription.Create(taskDescription));
             await repo.AddAsync(task);
-            await repo.SaveCreateChangesAsync();
+            await uow.SaveAsync(MutationKind.Create);
 
             var fromDb = await db.TaskItems.AsNoTracking().SingleAsync(t => t.Id == task.Id);
             fromDb.Title.Value.Should().Be(taskTitle);
@@ -70,6 +77,7 @@ namespace Infrastructure.Tests.Repositories
             using var dbh = new SqliteTestDb();
             await using var db = dbh.CreateContext();
             var repo = new TaskItemRepository(db);
+            var uow = new UnitOfWork(db);
 
             var (pId, lId, cId) = TestDataFactory.SeedLaneWithColumn(db);
             var task = TestDataFactory.SeedTaskItem(db, pId, lId, cId);
@@ -77,11 +85,16 @@ namespace Infrastructure.Tests.Repositories
             var tracked = await db.TaskItems.SingleAsync(t => t.Id == task.Id);
             var newDueDate = DateTimeOffset.UtcNow.AddDays(1);
             newDueDate = DateTimeOffset.FromUnixTimeMilliseconds(newDueDate.ToUnixTimeMilliseconds());
-            var (res, change) = await repo.EditAsync(task.Id, TaskTitle.Create("New"), TaskDescription.Create("NewD"), newDueDate, tracked.RowVersion!);
-            res.Should().Be(DomainMutation.Updated);
+            var (status, change) = await repo.EditAsync(
+                task.Id,
+                TaskTitle.Create("New"),
+                TaskDescription.Create("NewD"),
+                newDueDate,
+                tracked.RowVersion);
+            status.Should().Be(PrecheckStatus.Ready);
             change.Should().NotBeNull();
 
-            var saveRes = await repo.SaveUpdateChangesAsync();
+            var saveRes = await uow.SaveAsync(MutationKind.Update);
             saveRes.Should().Be(DomainMutation.Updated);
 
             var fromDb = await db.TaskItems.AsNoTracking().SingleAsync(t => t.Id == task.Id);
@@ -101,8 +114,13 @@ namespace Infrastructure.Tests.Repositories
             var task = TestDataFactory.SeedTaskItem(db, pId, lId, cId);
 
             var tracked = await db.TaskItems.SingleAsync(t => t.Id == task.Id);
-            var (res, change) = await repo.EditAsync(task.Id, null, null, null, tracked.RowVersion!);
-            res.Should().Be(DomainMutation.NoOp);
+            var (status, change) = await repo.EditAsync(
+                task.Id,
+                newTitle: null,
+                newDescription: null,
+                newDueDate: null,
+                tracked.RowVersion);
+            status.Should().Be(PrecheckStatus.NoOp);
             change.Should().BeNull();
         }
 
@@ -119,8 +137,13 @@ namespace Infrastructure.Tests.Repositories
             var taskB = TestDataFactory.SeedTaskItem(db, pId, lId, cId);
 
             var trackedB = await db.TaskItems.SingleAsync(x => x.Id == taskB.Id);
-            var (res, change) = await repo.EditAsync(taskB.Id, taskATitle, null, null, trackedB.RowVersion!);
-            res.Should().Be(DomainMutation.Conflict);
+            var (status, change) = await repo.EditAsync(
+                taskB.Id,
+                taskATitle,
+                newDescription: null,
+                newDueDate: null,
+                trackedB.RowVersion);
+            status.Should().Be(PrecheckStatus.Conflict);
             change.Should().BeNull();
         }
 
@@ -130,6 +153,7 @@ namespace Infrastructure.Tests.Repositories
             using var dbh = new SqliteTestDb();
             await using var db = dbh.CreateContext();
             var repo = new TaskItemRepository(db);
+            var uow = new UnitOfWork(db);
 
             // Board with two lanes and two columns
             var (pId, firstLaneId, firstColumnId) = TestDataFactory.SeedLaneWithColumn(db);
@@ -139,11 +163,17 @@ namespace Infrastructure.Tests.Repositories
             var task = TestDataFactory.SeedTaskItem(db, pId, firstLaneId, firstColumnId);
 
             var tracked = await db.TaskItems.SingleAsync(t => t.Id == task.Id);
-            var (res, change) = await repo.MoveAsync(task.Id, secondColumn.Id, secondLane.Id, pId, 5m, tracked.RowVersion!);
-            res.Should().Be(DomainMutation.Updated);
+            var (status, change) = await repo.MoveAsync(
+                task.Id,
+                targetColumnId: secondColumn.Id,
+                targetLaneId: secondLane.Id,
+                targetProjectId: pId,
+                targetSortKey: 5m,
+                tracked.RowVersion);
+            status.Should().Be(PrecheckStatus.Ready);
             change.Should().NotBeNull();
 
-            var saveRes = await repo.SaveUpdateChangesAsync();
+            var saveRes = await uow.SaveAsync(MutationKind.Update);
             saveRes.Should().Be(DomainMutation.Updated);
 
             var fromDb = await db.TaskItems.AsNoTracking().SingleAsync(t => t.Id == task.Id);
@@ -165,8 +195,14 @@ namespace Infrastructure.Tests.Repositories
             var task = TestDataFactory.SeedTaskItem(db, firstProjectId, firstLaneId, firstColumnId);
 
             var tracked = await db.TaskItems.SingleAsync(t => t.Id == task.Id);
-            var (res, change) = await repo.MoveAsync(task.Id, secondProjectColumnId, secondProjectLaneId, firstProjectId, 1m, tracked.RowVersion!);
-            res.Should().Be(DomainMutation.Conflict);
+            var (status, change) = await repo.MoveAsync(
+                task.Id,
+                targetColumnId: secondProjectColumnId,
+                targetLaneId: secondProjectLaneId,
+                targetProjectId: firstProjectId,
+                targetSortKey: 1m,
+                tracked.RowVersion);
+            status.Should().Be(PrecheckStatus.Conflict);
             change.Should().BeNull();
         }
 
@@ -198,45 +234,12 @@ namespace Infrastructure.Tests.Repositories
         }
 
         [Fact]
-        public async Task RebalanceSortKeysAsync_Normalizes_To_Sequential_From_Zero()
-        {
-            using var dbh = new SqliteTestDb();
-            await using var db = dbh.CreateContext();
-            var repo = new TaskItemRepository(db);
-
-            var (pId, lId, cId) = TestDataFactory.SeedLaneWithColumn(db);
-            var firstTaskTitle = "Task A";
-            var secondTaskTitle = "Task B";
-            var thirdTaskTitle = "Task C";
-            TestDataFactory.SeedTaskItem(db, pId, lId, cId, firstTaskTitle, sortKey: 20m);
-            TestDataFactory.SeedTaskItem(db, pId, lId, cId, secondTaskTitle, sortKey: 10m);
-            TestDataFactory.SeedTaskItem(db, pId, lId, cId, thirdTaskTitle, sortKey: 15m);
-            await repo.RebalanceSortKeysAsync(cId);
-
-            var order = await db.TaskItems
-                                .AsNoTracking()
-                                .Where(t => t.ColumnId == cId)
-                                .OrderBy(t => t.SortKey)
-                                .Select(t => t.Title.Value)
-                                .ToListAsync();
-
-            var keys = await db.TaskItems
-                                .AsNoTracking()
-                                .Where(t => t.ColumnId == cId)
-                                .OrderBy(t => t.SortKey)
-                                .Select(t => t.SortKey)
-                                .ToListAsync();
-
-            order.Should().Equal(secondTaskTitle, thirdTaskTitle, firstTaskTitle);
-            keys.Should().Equal(0m, 1m, 2m);
-        }
-
-        [Fact]
         public async Task DeleteAsync_Removes_TaskItem()
         {
             using var dbh = new SqliteTestDb();
             await using var db = dbh.CreateContext();
             var repo = new TaskItemRepository(db);
+            var uow = new UnitOfWork(db);
 
             var (pId, lId, cId) = TestDataFactory.SeedLaneWithColumn(db);
             var task = TestDataFactory.SeedTaskItem(db, pId, lId, cId);
@@ -244,8 +247,9 @@ namespace Infrastructure.Tests.Repositories
             var tracked = await db.TaskItems.SingleAsync(t => t.Id == task.Id);
 
             var res = await repo.DeleteAsync(tracked.Id, tracked.RowVersion!);
-            res.Should().Be(DomainMutation.Deleted);
+            res.Should().Be(PrecheckStatus.Ready);
 
+            await uow.SaveAsync(MutationKind.Delete);
             var exists = await db.TaskItems.AnyAsync(t => t.Id == task.Id);
             exists.Should().BeFalse();
         }
@@ -257,25 +261,8 @@ namespace Infrastructure.Tests.Repositories
             await using var db = dbh.CreateContext();
             var repo = new TaskItemRepository(db);
 
-            var res = await repo.DeleteAsync(Guid.NewGuid(), Array.Empty<byte>());
-            res.Should().Be(DomainMutation.NotFound);
-        }
-
-        [Fact]
-        public async Task DeleteAsync_Concurrency_Failure()
-        {
-            using var dbh = new SqliteTestDb();
-            await using var db = dbh.CreateContext();
-            var repo = new TaskItemRepository(db);
-
-            var (pId, lId, cId) = TestDataFactory.SeedLaneWithColumn(db);
-            var task = TestDataFactory.SeedTaskItem(db, pId, lId, cId);
-
-            var res = await repo.DeleteAsync(task.Id, Array.Empty<byte>());
-            res.Should().Be(DomainMutation.Conflict);
-
-            var stillExists = await db.TaskItems.AnyAsync(t => t.Id == task.Id);
-            stillExists.Should().BeTrue();
+            var res = await repo.DeleteAsync(Guid.NewGuid(), []);
+            res.Should().Be(PrecheckStatus.NotFound);
         }
     }
 }
