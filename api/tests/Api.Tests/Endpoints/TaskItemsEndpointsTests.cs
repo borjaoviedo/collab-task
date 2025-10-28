42f9314c5aa3d49b68e8219c9217aa72027d153d
borjaoviedo <borjaoviedo.dev@gmail.com>
2025-10-28 19:06:47 +0100

    refactor(tests-tasks-endpoints): use TaskItemTestHelper and BoardSetupHelper to simplify task CRUD and auth checks

--- api/tests/Api.Tests/Endpoints/TaskItemsEndpointsTests.cs
+++ api/tests/Api.Tests/Endpoints/TaskItemsEndpointsTests.cs
@@ -1,30 +1,44 @@
 using Api.Tests.Testing;
 using Application.TaskItems.DTOs;
+using Application.Users.DTOs;
 using FluentAssertions;
 using System.Net;
-using System.Net.Http.Headers;
-using System.Net.Http.Json;
-using TestHelpers.Api;
+using TestHelpers.Api.Auth;
+using TestHelpers.Api.Http;
+using TestHelpers.Api.Projects;
+using TestHelpers.Api.TaskItems;
 
 namespace Api.Tests.Endpoints
 {
     public sealed class TaskItemsEndpointsTests
     {
+
         [Fact]
         public async Task List_Empty_Create_GetById_Sends_ETag()
         {
             using var app = new TestApiFactory();
             using var client = app.CreateClient();
-            var auth = await EndpointsTestHelper.RegisterAndLoginAsync(client);
-            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", auth.AccessToken);
 
-            var (project, lane, column) = await EndpointsTestHelper.CreateProjectLaneAndColumn(client);
-            var list = await client.GetAsync($"/projects/{project.Id}/lanes/{lane.Id}/columns/{column.Id}/tasks");
-            list.StatusCode.Should().Be(HttpStatusCode.OK);
-            (await list.Content.ReadFromJsonAsync<TaskItemReadDto[]>(EndpointsTestHelper.Json))!.Should().BeEmpty();
+            await AuthTestHelper.RegisterLoginAndAuthorizeAsync(client);
 
-            var task = await EndpointsTestHelper.CreateTask(client, project.Id, lane.Id, column.Id);
+            // Create project, lane and column but no tasks
+            var (project, lane, column) = await BoardSetupHelper.CreateProjectLaneAndColumn(client);
+            var getTasksResponse = await TaskItemTestHelper.GetTaskItemsResponseAsync(
+                client,
+                project.Id,
+                lane.Id,
+                column.Id);
 
+            getTasksResponse.StatusCode.Should().Be(HttpStatusCode.OK);
+
+            // Task list is empty
+            var taskList = await getTasksResponse.ReadContentAsDtoAsync<List<TaskItemReadDto>>();
+            taskList.Should().BeEmpty();
+
+            // Create task
+            var task = await TaskItemTestHelper.PostTaskItemDtoAsync(client, project.Id, lane.Id, column.Id);
+
+            // Get task by id works
             var get = await client.GetAsync($"/projects/{project.Id}/lanes/{lane.Id}/columns/{column.Id}/tasks/{task.Id}");
             get.StatusCode.Should().Be(HttpStatusCode.OK);
             get.Headers.ETag.Should().NotBeNull();
@@ -36,17 +50,34 @@ namespace Api.Tests.Endpoints
             using var app = new TestApiFactory();
             using var client = app.CreateClient();
 
-            var list401 = await client.GetAsync($"/projects/{Guid.NewGuid()}/lanes/{Guid.NewGuid()}/columns/{Guid.NewGuid()}/tasks");
-            list401.StatusCode.Should().Be(HttpStatusCode.Unauthorized);
+            // Get tasks without being authenticated
+            var getTasksResponse = await TaskItemTestHelper.GetTaskItemsResponseAsync(
+                client,
+                projectId: Guid.NewGuid(),
+                laneId: Guid.NewGuid(),
+                columnId: Guid.NewGuid());
+            getTasksResponse.StatusCode.Should().Be(HttpStatusCode.Unauthorized);
 
-            var a = await EndpointsTestHelper.RegisterAndLoginAsync(client);
-            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", a.AccessToken);
-            var (prj, lane, col) = await EndpointsTestHelper.CreateProjectLaneAndColumn(client);
+            // Default user register, login and create project, lane and column
+            await AuthTestHelper.RegisterLoginAndAuthorizeAsync(client);
+            var (project, lane, column) = await BoardSetupHelper.CreateProjectLaneAndColumn(client);
 
-            var b = await EndpointsTestHelper.RegisterAndLoginAsync(client, name: "other", email: "b@x.com");
-            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", b.AccessToken);
-            var forbidden = await client.GetAsync($"/projects/{prj.Id}/lanes/{lane.Id}/columns/{col.Id}/tasks");
-            forbidden.StatusCode.Should().Be(HttpStatusCode.Forbidden);
+            // Different user register and login
+            var userRegisterDto = new UserRegisterDto()
+            {
+                Email = "random@e.com",
+                Name = "random",
+                Password = "Passw0rd!"
+            };
+            await AuthTestHelper.RegisterLoginAndAuthorizeAsync(client, userRegisterDto);
+
+            // Get tasks from project where different user is not a member: forbidden 
+            var forbiddenResponse = await TaskItemTestHelper.GetTaskItemsResponseAsync(
+                client,
+                project.Id,
+                lane.Id,
+                column.Id);
+            forbiddenResponse.StatusCode.Should().Be(HttpStatusCode.Forbidden);
         }
     }
 }
