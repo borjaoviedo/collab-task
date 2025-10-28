290808e8fb6de6fb06cea114bea9a3c76c4f1399
borjaoviedo <borjaoviedo.dev@gmail.com>
2025-10-28 19:06:00 +0100

    feat(test-helpers): add TaskItemDefaults with default values and create DTO preset

---
+++ api/tests/TestHelpers/Api/Defaults/TaskItemDefaults.cs
@@ -0,0 +1,20 @@
+using Application.TaskItems.DTOs;
+
+namespace TestHelpers.Api.Defaults
+{
+    public static class TaskItemDefaults
+    {
+        public readonly static string DefaultTaskTitle = "title";
+        public readonly static string DefaultTaskDescription = "description";
+        public readonly static DateTimeOffset? DefaultDueDate = null;
+        public readonly static decimal DefaultSortKey = 0m;
+
+        public readonly static TaskItemCreateDto DefaultTaskCreateDto = new()
+        {
+            Title = DefaultTaskTitle,
+            Description = DefaultTaskDescription,
+            DueDate = DefaultDueDate,
+            SortKey = DefaultSortKey
+        };
+    }
+}
