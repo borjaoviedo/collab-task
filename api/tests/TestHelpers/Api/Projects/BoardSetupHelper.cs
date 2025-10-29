421c4c0fcdba26cd2adfd323f1fe0f3fd1996355
borjaoviedo <borjaoviedo.dev@gmail.com>
2025-10-29 07:21:41 +0100

    refactor(test-helpers): rewrite BoardSetupHelper to accept DTOs, delegate to Project/Lane/Column/TaskItem helpers

--- api/tests/TestHelpers/Api/Projects/BoardSetupHelper.cs
+++ api/tests/TestHelpers/Api/Projects/BoardSetupHelper.cs
@@ -3,8 +3,12 @@ using Application.Columns.DTOs;
 using Application.Lanes.DTOs;
 using Application.Projects.DTOs;
 using Application.TaskItems.DTOs;
+using Application.TaskNotes.DTOs;
 using TestHelpers.Api.Auth;
 using TestHelpers.Api.Columns;
+using TestHelpers.Api.Lanes;
+using TestHelpers.Api.TaskItems;
+using TestHelpers.Api.TaskNotes;
 
 namespace TestHelpers.Api.Projects
 {
@@ -12,26 +16,23 @@ namespace TestHelpers.Api.Projects
     {
         public static async Task<(ProjectReadDto project, LaneReadDto lane)> CreateProjectAndLane(
             HttpClient client,
-            string projectName = "Project",
-            string laneName = "Lane",
-            int laneOrder = 0)
+            ProjectCreateDto? projectDto = null,
+            LaneCreateDto? laneDto = null)
         {
-            var project = await ProjectTestHelper.CreateProject(client, projectName);
-            var lane = await ProjectTestHelper.CreateLane(client, project.Id, laneName, laneOrder);
+            var project = await ProjectTestHelper.PostProjectDtoAsync(client, projectDto);
+            var lane = await LaneTestHelper.PostLaneDtoAsync(client, project.Id, laneDto);
 
             return (project, lane);
         }
 
         public static async Task<(ProjectReadDto project, LaneReadDto lane, ColumnReadDto column)> CreateProjectLaneAndColumn(
             HttpClient client,
-            string projectName = "Project",
-            string laneName = "Lane",
-            int laneOrder = 0,
-            string columnName = "Column",
-            int columnOrder = 0)
+            ProjectCreateDto? projectDto = null,
+            LaneCreateDto? laneDto = null,
+            ColumnCreateDto? columnDto = null)
         {
-            var (project, lane) = await CreateProjectAndLane(client, projectName, laneName, laneOrder);
-            var column = await ColumnTestHelper.PostColumnDtoAsync(client, project.Id, lane.Id, columnName, columnOrder);
+            var (project, lane) = await CreateProjectAndLane(client, projectDto, laneDto);
+            var column = await ColumnTestHelper.PostColumnDtoAsync(client, project.Id, lane.Id, columnDto);
 
             return (project, lane, column);
         }
@@ -44,33 +45,40 @@ namespace TestHelpers.Api.Projects
             AuthTokenReadDto user)>
             SetupBoard(
             HttpClient client,
-            string projectName = "Project",
-            string laneName = "Lane",
-            int laneOrder = 0,
-            string columnName = "Column",
-            int columnOrder = 0,
-            string taskTitle = "Task",
-            string taskDescription = "Description",
-            DateTimeOffset? taskDueDate = null)
+            ProjectCreateDto? projectDto = null,
+            LaneCreateDto? laneDto = null,
+            ColumnCreateDto? columnDto = null,
+            TaskItemCreateDto? taskDto = null)
         {
             var user = await AuthTestHelper.RegisterLoginAndAuthorizeAsync(client);
             var (project, lane, column) = await CreateProjectLaneAndColumn(
                 client,
-                projectName,
-                laneName,
-                laneOrder,
-                columnName,
-                columnOrder);
-            var task = await ProjectTestHelper.CreateTask(
+                projectDto,
+                laneDto,
+                columnDto);
+            var task = await TaskItemTestHelper.PostTaskItemDtoAsync(
                 client,
                 project.Id,
                 lane.Id,
                 column.Id,
-                taskTitle,
-                taskDescription,
-                taskDueDate);
+                taskDto);
 
             return (project, lane, column, task, user);
+        }
+
+        public static async Task<(
+            ProjectReadDto project,
+            LaneReadDto lane,
+            ColumnReadDto column,
+            TaskItemReadDto task,
+            TaskNoteReadDto note,
+            AuthTokenReadDto user)>
+            SetupBoardWithNote(HttpClient client)
+        {
+            var (project, lane, column, task, user) = await SetupBoard(client);
+            var note = await TaskNoteTestHelper.PostNoteDtoAsync(
+                client, project.Id, lane.Id, column.Id, task.Id);
+            return (project, lane, column, task, note, user);
         }
     }
 }
