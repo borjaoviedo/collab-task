35f8783e8041b894542ea52ba6eb847fac668bd8
borjaoviedo <borjaoviedo.dev@gmail.com>
2025-10-30 19:13:03 +0100

    docs(application): add XML documentation to PrecheckStatusExtensions and ToErrorDomainMutation method

--- api/src/Application/Common/Abstractions/Extensions/PrecheckStatusExtensions.cs
+++ api/src/Application/Common/Abstractions/Extensions/PrecheckStatusExtensions.cs
@@ -2,8 +2,21 @@ using Domain.Enums;
 
 namespace Application.Common.Abstractions.Extensions
 {
+    /// <summary>
+    /// Provides extension methods for converting <see cref="PrecheckStatus"/>
+    /// values into <see cref="DomainMutation"/> results.
+    /// </summary>
     public static class PrecheckStatusExtensions
     {
+        /// <summary>
+        /// Maps a <see cref="PrecheckStatus"/> to its corresponding <see cref="DomainMutation"/> error state.
+        /// </summary>
+        /// <param name="status">The precheck status returned by a repository operation.</param>
+        /// <returns>
+        /// The corresponding <see cref="DomainMutation"/> for <see cref="PrecheckStatus.NotFound"/>,
+        /// <see cref="PrecheckStatus.NoOp"/>, or <see cref="PrecheckStatus.Conflict"/>.
+        /// </returns>
+        /// <exception cref="InvalidOperationException">Thrown when the status value is not expected to map to an error.</exception>
         public static DomainMutation ToErrorDomainMutation(this PrecheckStatus status) =>
          status switch
          {
